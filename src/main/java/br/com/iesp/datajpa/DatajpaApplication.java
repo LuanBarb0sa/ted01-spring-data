63c129abd8a35151a875625411b59edca2df4e3a
breno.lima <brenoryan21@hotmail.com>
2023-10-02 23:37:25 -0300

    Update DatajpaApplication.java

--- src/main/java/br/com/iesp/datajpa/DatajpaApplication.java
+++ src/main/java/br/com/iesp/datajpa/DatajpaApplication.java
@@ -1,9 +1,13 @@
 package br.com.iesp.datajpa;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Backend 2 - UNIESP",
+		version = "1.0.0"))
 public class DatajpaApplication {
 
 	public static void main(String[] args) {
